96b27487b637efcd1803c667ca32d37d0761f75a
GuyEzra22 <117368131+GuyEzra22@users.noreply.github.com>
2022-12-09 03:59:58 +0200

    django

--- base/urls.py
+++ base/urls.py
@@ -4,9 +4,8 @@ from . import views
 
 urlpatterns = [
     path('',views.home, name="home"),
-    path('patient_login/', views.patient_login, name="patient_login"),
-    path('doctor_login/', views.doctor_login, name="doctor_login"),
+    path('patient_login/', views.loginPatient, name="patient_login"),
+    path('doctor_login/', views.loginDoctor, name="doctor_login"),
     path('manager_login/', views.manager_login, name="manager_login"),
-
     path('', views.style, name="style"),
 ]
--- base/views.py
+++ base/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.db.models import Q
+from django.contrib.auth.models import User
+from django.contrib import messages
+
 
 def home(request):
     return render(request, 'home.html')
@@ -6,13 +11,36 @@ def home(request):
 def style(request):
     return render(request, 'style.css')
 
-def patient_login(request):
-    return render(request, 'patient_login.html')
-
 def doctor_login(request):
     return render(request, 'doctor_login.html')
 
 def manager_login(request):
     return render(request, 'manager_login.html')        
 
+def loginPatient(request):
 
+    if request == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user =User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist')
+
+    context={}
+    return render(request, 'patient_login.html',context)
+
+def loginDoctor(request):
+
+    if request == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist')
+
+    context={}
+    return render(request, 'patient_login.html',context)
