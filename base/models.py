7a3f20e6b82ef32fb012a4394f467ec3ed372a26
maor atar <maoratar17@gmail.com>
2022-12-10 11:28:56 +0200

    New admin changes

--- base/models.py
+++ base/models.py
@@ -9,10 +9,14 @@ class Patient(models.Model):
     first_name = models.CharField(max_length=64)
     last_name = models.CharField(max_length=64)
     age = models.PositiveIntegerField()
+    address = models.CharField(max_length=64)
     disease = models.CharField(max_length=64)
     id_num = models.BigIntegerField()
     smoking = models.CharField(max_length=1, choices=SMOKE_CHOICES)
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
 
 class Doctor(models.Model):
     GENDER_CHOICES = (('M', 'Male'), ('F', 'Female'),)
@@ -25,3 +29,6 @@ class Doctor(models.Model):
     experience = models.PositiveIntegerField()
     id_num = models.BigIntegerField()
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
