d4c95edb0369f5bc799cc9b38eb8a258fa3403d4
maor atar <maoratar17@gmail.com>
2022-12-12 19:43:32 +0200

    add maor brach

---
+++ base/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path 
+from . import views
+
+
+urlpatterns = [
+    path('',views.home, name="home"),
+    path('signup/', views.signup, name="signup"),
+    path('signin/', views.signin, name="signin"),
+    path('signin/signout', views.signout, name="backhome"),
+    path('signout/', views.signout, name="signout"),
+    path('pharmacy/', views.pharmacy, name="pharmacy"),
+    path('blood_donation/', views.blood_donation, name="blood_donation"),
+    path('test/', views.test, name="test"),
+]
