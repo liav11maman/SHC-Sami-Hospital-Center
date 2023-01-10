bea218b9a5d19904a269a37d4ff12dae58b1fc4b
Maor Atar <maoratar17@gmail.com>
2023-01-10 22:02:36 +0200

    fixing main

--- base/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import Patient
-
- #--------------Edit Patient Form-------------- 
-class EditPatientForm(forms.ModelForm):
- 
-    class Meta:
-        model = Patient
-        fields = ["first_name", "last_name", "age", "address", "disease", "id_num", "smoking", "gender"]
