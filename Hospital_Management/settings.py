514ca11a3a265ddad5d0e3e67043a07584015b81
Maor Atar <maoratar17@gmail.com>
2023-01-13 19:07:43 +0200

    Pulling main

--- Hospital_Management/settings.py
+++ Hospital_Management/settings.py
@@ -85,10 +85,22 @@ WSGI_APPLICATION = 'Hospital_Management.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'TcRZk2O2bpKyji39hFd6',
+        'HOST': 'containers-us-west-171.railway.app',
+        'PORT': '7913',
     }
 }
 
